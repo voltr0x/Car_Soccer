50af09e55d4578835eab79cb12604f03d51e3325
voltr0x <virajshirodkar09@gmail.com>
2020-02-15 12:21:38 +0530

    Added player controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+
+    [SerializeField] float forwardSpeed;
+    [SerializeField] float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get player inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Move the vehicle
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed * verticalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    public GameObject enemyPrefab1;
+    public GameObject enemyPrefab2;
+    public GameObject enemyPrefab3;
+
+    private int enemyCount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Generate a random spawn position for obstacles & enemies
+    private Vector3 GenerateSpawnPos()
+    {
+        float spawnX = Random.Range(-12, 12);
+        float spawnZ = Random.Range(10, 20);
+
+        Vector3 randomPos = new Vector3(spawnX, 0, spawnZ);
+
+        return randomPos;
+    }
+}
